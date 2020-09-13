f3b1d8ca55097b15ea1f85a3009f62ad8fe835a4
Chris <ckeenan@starry.com>
2020-09-13 11:40:44 -0400

    Created LibraryConstants Kotlin file in buildSrc

---
+++ buildSrc/src/main/java/com.chrynan.logger.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.logger.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.logger"
+    const val owner = "chrynan"
+    const val repoName = "logger"
+    const val versionName = "1.2.1"
+    const val versionCode = 1
+    const val versionDescription = "Release 1.2.1"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/logger.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
