ec98794740831545ca759aff7200fcd7b9ad1474
Chris <ckeenan@starry.com>
2021-08-11 21:09:39 -0500

    Added typeName property to LogType

--- logger-core/src/commonMain/kotlin/com.chrynan.logger/LogType.kt
+++ logger-core/src/commonMain/kotlin/com.chrynan.logger/LogType.kt
@@ -1,11 +1,19 @@
+@file:Suppress("unused")
+
 package com.chrynan.logger
 
-enum class LogType {
+enum class LogType(val typeName: String) {
 
-    DEBUG,
-    WARNING,
-    INFO,
-    VERBOSE,
-    WTF,
-    ERROR
+    DEBUG(typeName = "Debug"),
+    WARNING(typeName = "Warning"),
+    INFO(typeName = "Info"),
+    VERBOSE(typeName = "Verbose"),
+    WTF(typeName = "Wtf"),
+    ERROR(typeName = "Error");
+
+    companion object {
+
+        fun getByTypeName(name: String, ignoreCase: Boolean = true): LogType? =
+            values().firstOrNull { it.typeName.equals(name, ignoreCase) }
+    }
 }
