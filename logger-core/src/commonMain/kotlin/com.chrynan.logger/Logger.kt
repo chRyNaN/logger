7d61376d08597d2d5479971288d5e1e0e77dc0a6
Chris <ckeenan@starry.com>
2021-08-11 21:38:04 -0500

    Updated Logger to implement LogInitializer

--- logger-core/src/commonMain/kotlin/com.chrynan.logger/Logger.kt
+++ logger-core/src/commonMain/kotlin/com.chrynan.logger/Logger.kt
@@ -5,9 +5,14 @@ package com.chrynan.logger
 /**
  * The base [Loggable] implementation that can be used globally once the [loggable] property is set.
  */
-object Logger : Loggable {
+object Logger : Loggable,
+    LogInitializer {
 
     lateinit var loggable: Loggable
+
+    override fun init() {
+        (loggable as? LogInitializer)?.init()
+    }
 
     override fun log(logType: LogType, tag: String, message: String?, throwable: Throwable?) =
         loggable.log(logType = logType, tag = tag, message = message, throwable = throwable)
