3ec7d56bf6b3606959a052c686f2e517330d20d3
Christopher Keenan <ckeenan@starry.com>
2022-01-14 13:49:35 -0600

    Updated Log.loggable to default to DefaultLogger()

--- logger-core/src/commonMain/kotlin/com.chrynan.logger/Log.kt
+++ logger-core/src/commonMain/kotlin/com.chrynan.logger/Log.kt
@@ -7,8 +7,8 @@ import kotlin.native.concurrent.ThreadLocal
 /**
  * An implementation of [Loggable] and [LogInitializer] that can be used as a singleton base to log
  * throughout the application. Before calling one of [log] functions, a [Loggable] has to be
- * assigned to the [loggable] property. Optionally, the [init] function can be called next, which
- * just delegates to the [loggable] init function if it is a [LogInitializer] implementation.
+ * assigned to the [loggable] property, this defaults to the [DefaultLogger]. Optionally, the [init] function can be
+ * called next, which just delegates to the [loggable] init function if it is a [LogInitializer] implementation.
  *
  * Note: This object is annotated with [ThreadLocal] which means that a new instance will be
  * created for each Thread in Kotlin Native.
@@ -17,7 +17,7 @@ import kotlin.native.concurrent.ThreadLocal
 object Log : Loggable,
     LogInitializer {
 
-    lateinit var loggable: Loggable
+    var loggable: Loggable = DefaultLogger()
 
     override fun init() {
         (loggable as? LogInitializer)?.init()
