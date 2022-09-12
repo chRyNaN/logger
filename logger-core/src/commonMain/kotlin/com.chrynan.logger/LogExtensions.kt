154a14300ccf81c68a36ee94410421225ff0db45
Christopher Keenan <ckeenan@starry.com>
2022-09-12 14:29:31 -0500

    Added messageBuilder overloaded Logger extension functions

--- logger-core/src/commonMain/kotlin/com.chrynan.logger/LogExtensions.kt
+++ logger-core/src/commonMain/kotlin/com.chrynan.logger/LogExtensions.kt
@@ -83,3 +83,69 @@ inline fun Logger.log(logValues: LogValues) =
  * Invokes the [Logger.log] function using the [Loggable.logValues] obtained from the provided [loggable] instance.
  */
 inline fun Logger.log(loggable: Loggable) = log(logValues = loggable.logValues())
+
+/**
+ * Invokes the [Logger.log] function for [LogType.ERROR].
+ */
+inline fun Logger.error(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.ERROR,
+        tag = tag ?: LogType.ERROR.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
+
+/**
+ * Invokes the [Logger.log] function for [LogType.DEBUG].
+ */
+inline fun Logger.debug(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.DEBUG,
+        tag = tag ?: LogType.DEBUG.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
+
+/**
+ * Invokes the [Logger.log] function for [LogType.WARNING].
+ */
+inline fun Logger.warning(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.WARNING,
+        tag = tag ?: LogType.WARNING.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
+
+/**
+ * Invokes the [Logger.log] function for [LogType.INFO].
+ */
+inline fun Logger.info(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.INFO,
+        tag = tag ?: LogType.INFO.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
+
+/**
+ * Invokes the [Logger.log] function for [LogType.VERBOSE].
+ */
+inline fun Logger.verbose(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.VERBOSE,
+        tag = tag ?: LogType.VERBOSE.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
+
+/**
+ * Invokes the [Logger.log] function for [LogType.WTF].
+ */
+inline fun Logger.wtf(tag: String? = null, throwable: Throwable? = null, messageBuilder: () -> String?) =
+    log(
+        logType = LogType.WTF,
+        tag = tag ?: LogType.WTF.typeName,
+        message = messageBuilder(),
+        throwable = throwable
+    )
