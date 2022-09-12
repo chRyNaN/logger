0532cffca5add3d291ca2f53f2a38be9bc10d8fd
Christopher Keenan <ckeenan@starry.com>
2022-09-12 16:14:53 -0500

    Added the Log.logValues property

--- logger-core/src/commonMain/kotlin/com.chrynan.logger/Log.kt
+++ logger-core/src/commonMain/kotlin/com.chrynan.logger/Log.kt
@@ -2,6 +2,8 @@
 
 package com.chrynan.logger
 
+import kotlinx.coroutines.flow.*
+
 /**
  * An implementation of [Logger] and [LogInitializer] that can be used as a singleton base to log
  * throughout the application. Before calling one of [log] functions, a [Logger] has to be
@@ -20,6 +22,23 @@ object Log : Logger,
     @Suppress("VARIABLE_IN_SINGLETON_WITHOUT_THREAD_LOCAL") // Use new Kotlin Native memory model.
     var logger: Logger = DefaultLogger()
 
+    /**
+     * Retrieves a [Flow] of [LogValues] that are emitted after every successful invocation of the [log] on this [Log]
+     * object.
+     *
+     * **Note:** If the [Log.isEnabled] property is `false` and the [Log.log] function is invoked, no [LogValues] will
+     * be emitted.
+     *
+     * **Note:** [LogValues] are emitted after the internal [Log.log] function is successfully invoked.
+     *
+     * **Note:** There is no guarantee that a value emitted from this [Flow] is actually logged, as it is up to the
+     * [Logger] implementation set as the [Log.logger] to decide how and whether to log.
+     */
+    val logValues: Flow<LogValues>
+        get() = values.asStateFlow().filterNotNull()
+
+    private val values = MutableStateFlow<LogValues?>(null)
+
     override fun init() {
         (logger as? LogInitializer)?.init()
     }
@@ -27,6 +46,13 @@ object Log : Logger,
     override fun log(logType: LogType, tag: String, message: String?, throwable: Throwable?) {
         if (logger.isEnabled) {
             logger.log(logType = logType, tag = tag, message = message, throwable = throwable)
+
+            values.value = LogValues(
+                logType = logType,
+                tag = tag,
+                message = message,
+                throwable = throwable
+            )
         }
     }
 }
